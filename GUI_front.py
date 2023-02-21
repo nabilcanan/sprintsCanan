a2109960254307c91f1d315ecf225b1ebfd875f1
nabilcanan <nabiicanan78@gmail.com>
2023-02-21 01:11:27 -0500

    initial

--- GUI_front.py
+++ GUI_front.py
@@ -11,16 +11,13 @@ entries = c.fetchall()
 root = tk.Tk()
 root.title("Database Entries")
 
-# create listbox to display entries
 listbox = tk.Listbox(root, width=100)
 listbox.pack(padx=100, pady=100)
 
-# add entries to listbox
 for entry in entries:
-    listbox.insert(tk.END, entry[1][:30])  # display first 30 characters of entry
+    listbox.insert(tk.END, entry[1][:30])
 
 
-# create function to display complete entry data
 def show_entry(event):
     # get selected entry
     index = listbox.curselection()[0]
